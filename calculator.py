8b87efe377ddc923c0c3671da05ec40d42d5d263
Harish madhavan <veeharish2005@gmail.com>
2024-02-14 13:36:34 +0530

    removed ttk and tkinter usage
    now fully leverages customtkinter

--- calculator.py
+++ calculator.py
@@ -1,6 +1,4 @@
-from tkinter import *
-import customtkinter 
-from tkinter import ttk
+import customtkinter
 
 #Create an instance of Tkinter frame
 win= customtkinter.CTk()
@@ -19,17 +17,17 @@ def display_text():
       label.configure(text=string1)
 
 #Initialize a Label to display the User Input
-label=Label(win, text="", font=("Courier 22 bold"))
+label=customtkinter.CTkLabel(master=win, text="", font=("Courier",22,"bold"))
 label.pack(anchor="center")
 label.place(x=250,y=100)
 
 #Create an Entry widget to accept User Input
-entry= Entry(win, width= 40)
+entry= customtkinter.CTkEntry(master=win, width= 350)
 entry.focus_set()
 entry.pack(anchor="n")
-entry.place(x=250,y=25)
+entry.place(x=200,y=25)
 
 #Create a Button to validate Entry Widget
-ttk.Button(win, text= "Okay",width= 20, command= display_text).place(x=300,y=200)
+button1= customtkinter.CTkButton(master= win, text= "Okay",width= 50, command= display_text).place(x=350,y=200)
 
 win.mainloop()
