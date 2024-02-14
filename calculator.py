66fde5359e54609d1dc35b556b24a0da85f30b32
Harish madhavan <veeharish2005@gmail.com>
2024-02-14 13:50:09 +0530

    removed useless lines and fixed an issue

--- calculator.py
+++ calculator.py
@@ -11,7 +11,9 @@ def display_text():
    global entry
    string= entry.get()
    if(string.isalpha() and string!="x"):
-      label.configure(text="enter numbers")
+      label.configure(text="Enter numbers")
+   elif(string==""):
+      label.configure(text="Enter any numbers")
    else:
       a=""
       for i in string:
@@ -24,16 +26,14 @@ def display_text():
 
 #Initialize a Label to display the User Input
 label=customtkinter.CTkLabel(master=win, text="", font=("Courier",22,"bold"))
-label.pack(anchor="center")
 label.place(x=250,y=100)
 
 #Create an Entry widget to accept User Input
 entry= customtkinter.CTkEntry(master=win, width= 350)
 entry.focus_set()
-entry.pack(anchor="n")
 entry.place(x=200,y=25)
 
 #Create a Button to validate Entry Widget
-button1= customtkinter.CTkButton(master= win, text= "Okay",width= 50, command= display_text).place(x=350,y=200)
+button1= customtkinter.CTkButton(master= win, text= "=",width= 50, command= display_text).place(x=350,y=200)
 
 win.mainloop()
